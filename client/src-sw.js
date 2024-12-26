349aa5cdcf34ddd58fac00f902daf81de18955d5
unknown <tina.rahimian94@gmail.com>
2024-12-25 20:23:54 -0800

    final commit

--- client/src-sw.js
+++ client/src-sw.js
@@ -28,7 +28,7 @@ registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 //  Implement asset caching
 registerRoute(
-  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+  ({ request }) => ['style', 'script', 'image'].includes(request.destination),
   new StaleWhileRevalidate({
     cacheName: 'asset-cache',
     plugins: [
